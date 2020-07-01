d9d98952851301615cb0ea7b353ee9dd7a41b0b0
liangdrew <liangdrew@gmail.com>
2020-07-01 19:40:57 -0400

    Support using existing aliases to define new aliases

--- test/simulation/TestAliasManager.java
+++ test/simulation/TestAliasManager.java
@@ -14,13 +14,13 @@ public class TestAliasManager {
         String alias2 = "f2";
         String formula = "a=b";
 
-        am.addAlias(alias, formula);
+        assertTrue(am.addAlias(alias, formula));
         assertTrue(am.isAlias(alias));
         assertEquals(formula, am.getFormula(alias));
         assertEquals(null, am.getFormula(alias2));
         assertFalse(am.isAlias(alias2));
 
-        am.addAlias(alias2, formula);
+        assertTrue(am.addAlias(alias2, formula));
         assertTrue(am.isAlias(alias));
         assertTrue(am.isAlias(alias2));
         assertEquals(formula, am.getFormula(alias));
@@ -28,11 +28,66 @@ public class TestAliasManager {
 
         // Test overwriting.
         String formula2 = "c=d";
-        am.addAlias(alias2, formula2);
+        assertTrue(am.addAlias(alias2, formula2));
         assertTrue(am.isAlias(alias));
         assertTrue(am.isAlias(alias2));
         assertEquals(formula, am.getFormula(alias));
         assertEquals(formula2, am.getFormula(alias2));
+    }
+
+    @Test
+    public void testAddAlias_nested() {
+        AliasManager am = new AliasManager();
+
+        assertTrue(am.addAlias("a", "1"));
+        assertTrue(am.isAlias("a"));
+        assertEquals("1", am.getFormula("a"));
+
+        assertTrue(am.addAlias("b", "`a`+1"));
+        assertTrue(am.isAlias("b"));
+        assertEquals("1+1", am.getFormula("b"));
+
+        assertTrue(am.addAlias("c", "`a`+`b`+1"));
+        assertTrue(am.isAlias("c"));
+        assertEquals("1+1+1+1", am.getFormula("c"));
+
+        assertTrue(am.addAlias("d", "`a `+1"));
+        assertTrue(am.isAlias("d"));
+        assertEquals("1+1", am.getFormula("d"));
+
+        assertTrue(am.addAlias("e", "` a`+1"));
+        assertTrue(am.isAlias("e"));
+        assertEquals("1+1", am.getFormula("e"));
+
+        assertTrue(am.addAlias("f", "``+1"));
+        assertTrue(am.isAlias("f"));
+        assertEquals("+1", am.getFormula("f"));
+
+        assertTrue(am.addAlias("abcd", "2"));
+        assertTrue(am.isAlias("abcd"));
+        assertEquals("2", am.getFormula("abcd"));
+
+        assertTrue(am.addAlias("g", "`abcd`+1"));
+        assertTrue(am.isAlias("g"));
+        assertEquals("2+1", am.getFormula("g"));
+
+        assertTrue(am.addAlias("h", "1+`abcd`"));
+        assertTrue(am.isAlias("h"));
+        assertEquals("1+2", am.getFormula("h"));
+
+        assertTrue(am.addAlias("i", "`abcd`"));
+        assertTrue(am.isAlias("i"));
+        assertEquals("2", am.getFormula("i"));
+
+        assertFalse(am.addAlias("z", "`a``+1"));
+        assertFalse(am.isAlias("z"));
+
+        assertFalse(am.isAlias("foo"));
+        assertFalse(am.addAlias("z", "`foo`+1"));
+        assertFalse(am.isAlias("z"));
+
+        assertFalse(am.addAlias("z", "``"));
+        assertFalse(am.isAlias("z"));
     }
 
     @Test
@@ -42,7 +97,7 @@ public class TestAliasManager {
         String formula = "a=b";
 
         assertFalse(am.removeAlias(alias));
-        am.addAlias(alias, formula);
+        assertTrue(am.addAlias(alias, formula));
         assertTrue(am.removeAlias(alias));
         assertFalse(am.isAlias(alias));
         assertFalse(am.removeAlias(alias));
@@ -55,8 +110,8 @@ public class TestAliasManager {
         String alias2 = "f2";
         String formula = "a=b";
 
-        am.addAlias(alias, formula);
-        am.addAlias(alias2, formula);
+        assertTrue(am.addAlias(alias, formula));
+        assertTrue(am.addAlias(alias2, formula));
         am.clearAliases();
         assertFalse(am.isAlias(alias));
         assertFalse(am.isAlias(alias2));
@@ -69,8 +124,8 @@ public class TestAliasManager {
         String alias2 = "f2";
         String formula = "a=b";
 
-        am.addAlias(alias, formula);
-        am.addAlias(alias2, formula);
+        assertTrue(am.addAlias(alias, formula));
+        assertTrue(am.addAlias(alias2, formula));
         String expected = "\nAlias           Formula\nf1              a=b\nf2              a=b\n";
         assertEquals(expected, am.getFormattedAliases());
     }
